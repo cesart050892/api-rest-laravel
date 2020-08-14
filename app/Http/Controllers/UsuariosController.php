9cbbf4323383ebbbe2c3e52775b55c6d542c1a3b
cesart050892 <cesart050892@gmail.com>
2020-08-14 00:26:48 -0600

    Model and Controller

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
+//Modelo
 use App\Usuarios;
+
 class UsuariosController extends Controller
 {
     //Metodos Http
@@ -14,45 +16,69 @@ class UsuariosController extends Controller
     public function index()
     {
         $json = array(
-            "Detalle"=>"No encontrado"
+            'API REST LARAVEL' => [
+                'Header' => [
+                    'Status' => http_response_code(),
+                    'request' => [
+                        'field00' => 'nombre',
+                        'field01' => 'apellido',
+                        'field02' => 'email'
+                    ],
+                    'url' => 'apirest-laravel.com'
+                ],
+                'Detalle' => 'Ingrese a la url /registro'
+            ]
         );
-        return json_encode($json,true);
+        //return json_encode($json, true);
+        return $json;
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
+
         $datos = array(
-            "nombre"=>$request->input("nombre"),
-            "apellido"=>$request->input("apellido"),
-            "correo"=>$request->input("email")
+            "nombre" => $request->input("nombre"),
+            "apellido" => $request->input("apellido"),
+            "correo" => $request->input("email")
         );
 
-        $validator = Validator::make($datos, [
-            'nombre' => 'required|string|max:255',
-            'apellido' => 'required|string|max:255',
-            'correo' => 'required|string|email|unique:usuario|max:255'
-        ]);
+        if (!empty($datos)) {
 
-        if ($validator->fails()) {
+            $validator = Validator::make($datos, [
+                'nombre' => 'required|string|max:255',
+                'apellido' => 'required|string|max:255',
+                'correo' => 'required|string|email|unique:usuario|max:255'
+            ]);
+
+            if ($validator->fails()) {
+                $json = array(
+                    "Datos" => "No validos, con errores"
+                );
+                //return json_encode($json, true);
+                return $json;
+            } else {
+
+                $usuario = new Usuarios();
+                $usuario->nombre = $datos['nombre'];
+                $usuario->apellido = $datos['apellido'];
+                $usuario->correo = $datos['correo'];
+                $usuario->id_usuario = Hash::make($datos['nombre'] . $datos['apellido'] . $datos['correo']);
+                $usuario->llave_secreta = Hash::make($datos['correo'] . $datos['apellido'] . $datos['nombre'], ['rounds' => 12,]);
+
+                $usuario->save();
+
+                $json = array(
+                    "Registros" => "Guardados!"
+                );
+                //return json_encode($json, true);
+                return $json;
+            }
+        } else {
             $json = array(
-                "Datos"=>"No validos"
+                "Registros" => "Vacios!"
             );
-            return json_encode($json,true);
-        }else{
-
-            $usuario = new Usuarios();
-            $usuario->nombre = $datos['nombre'];
-            $usuario->apellido =$datos['apellido'];
-            $usuario->correo = $datos['correo'];
-            $usuario->id_usuario = Hash::make($datos['nombre'].$datos['apellido'].$datos['correo']);
-            $usuario->llave_secreta = Hash::make($datos['correo'].$datos['apellido'].$datos['nombre'],['rounds' => 12,]);
-
-            $usuario->save();
-
-            $json = array(
-                "Registro"=>"Exitoso!"
-            );
-            return json_encode($json,true);
+            //return json_encode($json, true);
+            return $json;
         }
-
     }
 }
---
+++ app/Modelo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Modelo extends Model
+{
+    //Nombre de la tabla
+    protected $table = 'modelo';
+}
